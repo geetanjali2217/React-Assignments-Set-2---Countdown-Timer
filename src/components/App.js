e4775d20a3cfa97e3066b0845f1fe31967fae1a0
Geetanjali Katare <68340329+geetanjali2217@users.noreply.github.com>
2020-12-02 21:40:18 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,18 +3,47 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
+  const [remTime, setRemTime] = useState();
+
+  const handelKeyDown = (event) => {
+    const value = event.target.value;
+    console.log(value);
+    if (event.keyCode === 13) {
+      if (!isNaN(value) && parseInt(value)>=0) {
+        setRemTime(value);
+      } else setRemTime(0);
+    }
+  };
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      if (remTime && remTime > 0) {
+        const reduceTime = remTime - 1;
+        setRemTime(reduceTime);
+      }
+    }, 1000);
+
+  return () => {
+      clearInterval(timer);
+    };
+  });
 
   return (
     <div className="wrapper">
       <div id="whole-center">
-        <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
-        </h1>
+        <div>
+          Reverse countdown for
+          <input
+            id="timeCount"
+            onKeyDown={(e) => handelKeyDown(e)}
+          />{" "}
+          sec.
+        </div>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{remTime}</div>
     </div>
-  )
-}
+  );
+};
 
 
 export default App;
